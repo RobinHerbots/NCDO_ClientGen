c4ff70ee933b41c4776280fc5209f80530527725
Robin Herbots <robin.herbots@gmail.com>
2018-10-25 17:10:35 +0200

    allow to specify the target framework

--- bin/ncdo_clientgen.js
+++ bin/ncdo_clientgen.js
@@ -6,16 +6,17 @@ var cg = require('../lib/clientgen'),
 if (process.argv[2] !== undefined) {
     var catalogUrl = process.argv[2],
         outputpath = process.argv[3] || process.cwd(),
-        noclean = process.argv[4] || false;
+        noclean = process.argv[4] || false,
+        framework = process.argv[5];
 
     outputpath = path.relative(process.cwd(), outputpath);
 
-    cg(catalogUrl, outputpath, noclean).then(function (data) {
+    cg(catalogUrl, outputpath, noclean, framework).then(function (data) {
         console.log(data);
     }).error(function (data) {
         console.log(data);
     });
 } else {
-    console.log("usage: ncdo_clientgen <catalogUrl> [<outputPath>, <noclean>] ");
+    console.log("usage: ncdo_clientgen <catalogUrl> [<outputPath>] [<noclean> <framework>]");
 }
 
