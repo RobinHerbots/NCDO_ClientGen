21c41cef2183c65280d577fd8b33e043a26871f9
Robin Herbots <robin.herbots@gmail.com>
2019-02-11 11:50:31 +0100

    update build tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,13 @@
-var sep = require('path').sep;
+var sep = require('path').sep,
+    child_process = require('child_process');
 
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
+        release: {
+            options: {
+                additionalFiles: ['grunt-ncdo-clientgen/package.json']
+            }
+        },
         availabletasks: {
             tasks: {
                 options: {
@@ -16,4 +21,22 @@ module.exports = function (grunt) {
 // Load the plugin that provides the tasks.
     require('load-grunt-tasks')(grunt);
     grunt.registerTask('default', ["availabletasks"]);
+    grunt.registerTask("release_grunt", "Release grunt plugin for ncdo_clientgen", function () {
+        var done = this.async(), pkg = grunt.file.readJSON("grunt-ncdo-clientgen/package.json");
+        pkg.dependencies.ncdo_clientgen = pkg.version;
+        grunt.file.write("grunt-ncdo-clientgen/package.json", JSON.stringify(pkg));
+
+        child_process.execSync(`npm publish grunt-ncdo-clientgen`, function (error, stdout, stderr) {
+                if (error) {
+                    console.error("exec error: " + error);
+                    return;
+                }
+
+                console.log("stdout: " + stdout);
+                console.log("stderr: " + stderr);
+                done();
+            }
+        );
+    });
+    grunt.registerTask('publish', ["release", "release_grunt"]);
 };
